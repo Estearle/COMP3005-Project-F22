d4bed29189195ee1ff92b1590f2dea3ba029cbdb
rachelwongcl <119693648+rachelwongcl@users.noreply.github.com>
2022-12-06 20:17:35 -0500

    updated server

---
+++ JS/public/js/search.js
@@ -0,0 +1,59 @@
+let addBtn = document.getElementById("add");
+addBtn.onclick = addBook;
+
+function addBook(){
+    let bookName = document.getElementById("bkName").value;
+    let isbn =document.getElementById('isbn').value;
+    let author = document.getElementById("author").value;
+    let publisher = document.getElementById("publisher").value;
+    let pages = Number(document.getElementById("pages").value);
+    let price = Number(document.getElementById("price").value);
+    let stock = Number(document.getElementById("stock").value);
+    let cost = Number(document.getElementById("cost").value);
+    let errorMsg ="";
+
+    //check the input
+    //if the input is incorrect, alert the owner
+    if(nameItem.length !== 0 && isbn.length === 13 && author.length !== 0 && publisher.length !== 0 && !isNaN(page) && !isNaN(price) && !isNaN(stock) && !isNaN(cost) && document.getElementById("price").value.legnth !== 0  && document.getElementById("stock").value.length !==0 && document.getElementById("cost").value.length !==0 && document.getElementById("page").value.length !==0){
+        let newBook = {'isbn':isbn, 'bookname':bookName , 'pages':pages ,'price':price , 'stock':stock , 'publisher':publisher,'pages':pages,'price':price,'stock':stock,'cost':cost};
+        let xhttp = new XMLHttpRequest()
+        xhttp.onreadystatechange = function() { 
+            console.log("Add New Book");
+            if (this.readyState == 4 && this.status == 200) {
+                alert("Success!");
+            }
+        }
+    //Send a POST request to the server with the new book information
+    xhttp.open("POST", `/books/${isbn}`);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(JSON.stringify(newBook))
+    }
+    else{
+        if(bookName.length === 0){
+            errorMsg +="Please enter the book name.\n";
+        }
+        if(isbn.length !== 13){
+            errorMsg +="Incorrect ISBN.\n";
+        }
+        if(author.length !== 0){
+            errorMsg += "Please enter the name of the author.\n";
+        }
+        if(publisher.length !== 0){
+            errorMsg += "Please enter the name of the publisher.\n";
+        }
+        if(isNaN(parseInt(document.getElementById("pages").value))){
+            errorMsg += "Please enter the number of page.\n";
+        }
+        if(isNaN(parseFloat(document.getElementById("price").value))){
+            errorMsg += "Please enter a number for the price.\n";
+        }
+        if(isNaN(parseInt(document.getElementById("stock").value))){
+            errorMsg +="Please enter a number for stock.\n";
+        }
+        if(isNaN(parseInt(document.getElementById("cost").value))){
+            errorMsg += "Please enter a number for cost.\n";
+        }
+        alert(errorMsg);
+        return;
+    }
+}
--- JS/server.js
+++ JS/server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const fs = require("fs");
 const app = express();
 const {Pool, Client} = require("pg");
-
 const PORT = process.env.PORT || 3000
 const ROOT_DIR_JS = '/public/js'; //root directory for javascript files
-
+let books;
 //Provide static server
 app.use(express.static(__dirname + ROOT_DIR_JS));
 
@@ -16,18 +15,78 @@ app.use(express.json())
 app.set('views','./views');
 app.set('view engine','pug');
 
+//connect server
+//
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'bookstore',
+  password: 'password',
+  port: 5432,
+})
+
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  app.listen(PORT);
+  console.log("Server listening on port 3000");
+});
 
 
 //Login Page
 app.get('/',(req,res)=>{
-    res.render('login');
+  res.status(200).render('login');
 })
 
 // Loads all the books 
 // Search 
-app.get('/books',(req,res)=>{
-    res.render('books',{})
+app.get('/books',(req,response)=>{
+  client.query('SELECT * FROM public.books',(err,res)=>{
+    if(err){
+      respopnse.status(404);
+    }
+    console.log(res.rows);
+    books = res.rows;
+    response.status(200).render('books',{books,books});
+  })
+  
 })
+
+//Specific Books(GET)
+app.get('/books/:ISBN',(req,response)=>{
+  let obj_id = req.params.ISBN;
+  let book,genre;
+  client.query(`SELECT * FROM books WHERE isbn='${obj_id}'`,(err,res)=>{
+    if(err){
+      response.status(404);
+    }
+    book = res.rows;
+    client.query(`SELECT genre FROM bookgenres WHERE isbn='${obj_id}'`,(err,r)=>{
+      if(err){
+        response.status(404);
+      }
+      genre = r.rows;
+      response.status(200).render('book',{book:book,genre:genre});
+    })
+    console.log(book)
+    console.log("GENRE:"+genre)
+    
+  })  
+})
+  
+//add book
+app.get("/add",(req,res)=>{
+  res.render("add");
+})
+app.post('/books/:ISBN',(req,response)=>{
+  let newBook=req.body;
+  console.log(JSON.stringify(newBook));
+  client.query(`insert into books values ('${newBook.isbn}', ${newBook.bookname}, ${newBook.pages}, ${newBook.price}, ${newBook.stock}, 0, ${newBook.publisher}, ${newBook.cost},0)`,(err,res)=>{
+    console.log(err,res);
+  })
+})
+
+
 
 //Cart
 app.get('order',(req,res)=>{
@@ -44,21 +103,4 @@ app.get('/report',(req,res)=>{
 
 
 
-//connect server
-
-
-const client = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: '‘password’ ',
-  port: 5433,
-  // ssl  : {
-  //   ca : fs.readFileSync('<path to CA cert file>')
-  // }
-})
-client.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
 
