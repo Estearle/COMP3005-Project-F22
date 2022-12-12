e8a8db0cce7f6839d4f1ac364a277577da950500
rachelwongcl <119693648+rachelwongcl@users.noreply.github.com>
2022-12-11 23:05:34 -0500

    Finished queries.sql

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -68,4 +68,83 @@ SELECT *
 FROM public.customers
 WHERE customers.uname = 'user_username'
 
+--final request to finalize order with all the appropriate information
+14) Retrieve all the orders 
+SELECT * 
+FROM public.orders
 
+-- Insert the order if successful
+15) Insert into order 
+INSERT INTO orders(ordernumber,billinginfo,shippinginfo,trackinginfo,customer) 
+VALUES($1,$2,$3,$4,$5)
+
+--We also add to the information to orders database 
+16) Insert the new order into bookorders database
+INSERT INTO bookorders(ordernumber, isbn, numbersold) 
+VALUES($1,$2,$3)
+
+--Update the book relation with new stock, number sold and if need to restock
+17) Update book 
+UPDATE books 
+SET stock = '${stock}', numbersold = '${sold}', restock = '${restock}' 
+WHERE isbn = '${id}'
+
+-- Update the genre sales value in genres relation
+18) Update genres 
+UPDATE genres 
+SET sales = sales + '${books[id].add}', totalsales = totalsales + '${books[id].add * books[id].price}' 
+WHERE genre = '${genres[i]}'
+
+--Update the author sales values in authors relation
+19)Update author sales
+UPDATE authors 
+SET sales = sales + '${books[id].add}', totalsales = totalsales + '${books[id].add * books[id].price}' 
+WHERE author = '${authors[i]}'
+
+-- Get finances page request to show all the expenditures 
+20) Retrieve info from all books
+SELECT *
+FROM public.books
+
+-- Show all the sales for each genre
+21) Retrieve info from all genres
+SELECT * 
+FROM public.genres
+
+--Show all the sales for each author
+22) Retrieve info from all authors 
+SELECT * 
+FROM public.authors
+
+-- Owner's side: get all the books info
+23) Retrieve info from all books
+SELECT * 
+FROM public.books
+
+24) Retrieve info from all book genre 
+SELECT * 
+FROM bookgenres
+
+25)Retrieve info from all book authors
+SELECT * 
+FROM bookauthors 
+
+-- Delete books when owner requests
+24-27) Delete books and its authors and genres information 
+DELETE FROM bookorders 
+WHERE isbn='${isbn}'
+
+DELETE FROM bookauthors 
+WHERE isbn='${isbn}'
+
+DELETE FROM bookgenres 
+WHERE isbn='${isbn}'
+
+DELETE FROM books 
+WHERE isbn='${isbn}'
+
+--Search Tracking number 
+28) Retrieve the info from a specified tracking number
+SELECT * 
+FROM orders 
+WHERE TrackingInfo='_speicified_tracking_num'
